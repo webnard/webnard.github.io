3a1b07bbd04d68c92791a336835b41e0f32d766b
Ian Hunter <beanland2@gmail.com>
2013-05-27 13:26:26 -0600

    Able to make combined css.

--- _src/build.js
+++ _src/build.js
@@ -1,44 +1,60 @@
-var jQuery = "http://code.jquery.com/jquery-latest.js";
 var fs = require("fs");
+var jQuery = fs.readFileSync(__dirname + "/jquery.js").toString();
 var jsdom = require("jsdom");
 var yui = require("yuicompressor");
 
 var siteDir = __dirname + '/site';
-var baseDir = __dirname + '/../';
+var baseDir = __dirname + '/..';
+var cssDir = baseDir + '/../css';
 var tplDir = __dirname + '/templates';
 
-var tmpJs = __dirname + '/app.css.tmp';
+var tmpCss = __dirname + '/app.css.tmp';
 var tmpJs = __dirname + '/app.js.tmp';
 
-jsdom.defaultDocumentFeatures = {
-    FetchExternalResources: false,
-    ProcessExternalResources: false
-};
+var unlinkFiles = [];
 
 // make a copy of the template file with minified data
 fs.readdir(tplDir, function(err, files) {
     files.forEach(function(file) {
+        if(file.slice(-4) == ".tmp") {
+            return;
+        }
+
         // make a copy
         var tmpFile = tplDir + '/' + file + '.tmp';
-        var id = fs.openSync(tmpFile,'w');
         var copy = fs.readFileSync(tplDir + '/' + file);
-        console.log(copy);
-        return;
-        fs.writeSync(id, copy);
-        fs.closeSync(id);
+        fs.writeFileSync(tmpFile, copy);
 
-        combineJs(tmpFile);
         combineCss(tmpFile);
-
-        //fs.unlink(tmpFile, function(){});
+        
+        unlinkFiles.push(tmpFile);
     });
 });
 
-fs.readdir(siteDir, function(err, files) {
-    files.forEach(function(file){
-         
-    });
-});
+
+function templateFile(file, template) {
+    if(fs.isDir(file)) {
+        fs.readdir(file, function(err, files) {
+            files.forEach(function(f){
+                templateFile(file);
+            });
+        });
+        return;
+    }
+
+     var contents = fs.readFileSync(file).toString();
+     var templateContents = fs.readFileSync(template);
+
+     jsdom.env({
+        html: templateContents,
+        src: [jQuery],
+        done: function(errors, window) {
+            var $ = window.jQuery;
+            $("content").replaceWith(contents);
+            console.log(contents);
+        }
+     });
+}
 
 function isLocal(filename) {
     return filename.indexOf("http://") !== 0 
@@ -46,60 +62,30 @@ function isLocal(filename) {
            && filename.indexOf("https://") !== 0;
 }
 
-function combineJs(file) {
-    jsdom.env({
-        html: file,
-        scripts: [jQuery],
-        done: function(errors, window) {
-            var $ = window.$;
-            var js = $("script[src]");
-            var g = fs.openSync(tmpJs, 'w');
-            var replaced = false;
-
-            js.each(function() {
-               var script = $(this).attr('src');
-
-               if(!isLocal(script)) {
-                    return;
-               }
-               var data = fs.readFileSync(script);
-               fs.writeSync(g,data);
-               
-               if(!replaced) {
-                    $(this).replace("<script src='js/app.min.js'></script>");
-                    replaced = true;
-               }
-               else
-               {
-                    $(this).remove();
-               }
-            });
-            fs.closeSync(g);
-        }
-    });
-}
-
 function combineCss(file) {
+    var data = fs.readFileSync(file).toString();
     jsdom.env({
-        html: file,
-        scripts: [jQuery],
+        html: data,
+        src: [jQuery],
         done: function(errors, window) {
-            var $ = window.$;
-            var el = $("link[type=stylesheet]");
+            var $ = window.jQuery;
+            var el = $("link[rel=stylesheet]");
             var g = fs.openSync(tmpCss, 'w');
+            fs.closeSync(g);
             var replaced = false;
-
+            
             el.each(function() {
                var css = $(this).attr('href');
-
                if(!isLocal(css)) {
                     return;
                }
+               css = __dirname + "/" + css;
+                
                var data = fs.readFileSync(css);
-               fs.writeSync(g,data);
+               fs.appendFileSync(tmpCss, data);
                
                if(!replaced) {
-                    $(this).replace("<link rel='stylesheet' href='css/app.min.css'>");
+                    $(this).replaceWith("<link rel='stylesheet' href='/css/app.min.css'>");
                     replaced = true;
                }
                else
@@ -107,7 +93,9 @@ function combineCss(file) {
                     $(this).remove();
                }
             });
-            fs.closeSync(g);
+            
+            fs.rename(tmpCss, cssDir + '/app.min.css', function(){});
+            fs.writeFileSync(file, window.document.doctype.toString() + window.document.innerHTML);
         }
     });
 }
